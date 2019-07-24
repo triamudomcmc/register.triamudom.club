3247302d1d7f3a620284de0e1807e34dfd35850e
iammarkps <markkung08@gmail.com>
2019-07-24 22:55:04 +0700

    :bug: Fix type

--- server/models/audition.go
+++ server/models/audition.go
@@ -10,7 +10,7 @@ type Audition struct {
 	ID int `gorm:"UNIQUE;NOT NULL;PRIMARY_KEY;AUTO_INCREMENT"`
 	ClubID string
 	Club Club
-	UserID string
+	UserID uint16
 	User User
 	Status string
 	UpdatedAt time.Time

8cb56a0e8065893912c6e2199538920d2bff23b9
iammarkps <markkung08@gmail.com>
2019-09-15 11:28:12 +0700

    :sparkles: Implement authentication

--- server/app/app.go
+++ server/app/app.go
@@ -11,26 +11,22 @@ import (
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/middleware"
 	"gopkg.in/boj/redistore.v1"
+
 	"log"
 	"net/http"
 	"os"
 )
-
-func newRedisStore() sessions.Store {
-	store, err := redistore.NewRediStore(10, "tcp", ":6379", "", []byte(os.Getenv("SESSION_KEY")))
-	if err != nil {
-		log.Fatal(err)
-	}
-	return store
-}
 
 func New() (*echo.Echo, *gorm.DB) {
 	e := echo.New()
 
 	dbURI := "root:Mark1203@tcp(127.0.0.1:3306)/crn?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open("mysql", dbURI)
+	log.Printf("Connecting to database...")
 	if err != nil {
 		log.Fatal("Could not connect database")
+	} else {
+		log.Printf("Successfully connected to database")
 	}
 	defer db.Close()
 
@@ -43,15 +39,28 @@ func New() (*echo.Echo, *gorm.DB) {
 	e.Use(middleware.Secure())
 	e.Use(session.Middleware(newRedisStore()))
 
+	e.Debug = true
+
 	h := &handler.Handler{DB: db}
 
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "😼Triam Udom Suksa School's club registration API is running!")
 	})
 	e.POST("/login", h.Login)
-	e.POST("/logout", h.Logout)
+	e.GET("/logout", h.Logout)
 	e.GET("/health", h.HealthCheck)
 
 	return e, db
+}
+
+func newRedisStore() sessions.Store {
+	store, err := redistore.NewRediStore(10, "tcp", ":6379", "", []byte(os.Getenv("SESSION_KEY")))
+	log.Printf("Connecting to redis...")
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("Successfully connected to redis")
+	}
+	return store
 }
 

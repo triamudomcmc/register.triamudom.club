2262274ac73e0d066cd521dee4e61554f49be449
iammarkps <markkung08@gmail.com>
2020-07-16 13:04:13 +0700

    :recycle: Move font face to tailwind config file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -50,6 +50,23 @@ module.exports = {
               'url("/assets/fonts/Inter/Inter-italic.var.woff2?3.13") format("woff2")',
           },
         },
+        {
+          '@font-face': {
+            fontFamily: 'Kanit',
+            fontStyle: 'normal',
+            fontWeight: '400',
+            fontDisplay: 'swap',
+            src:
+              "local('Kanit Regular'), local('Kanit-Regular'),url('https://fonts.gstatic.com/s/kanit/v5/nKKZ-Go6G5tXcraBGwCKd6xBDFs.woff2')format('woff2')",
+            unicodeRange: 'U+0E01-0E5B, U+200C-200D, U+25CC',
+          },
+          '@font-face': {
+            fontFamily: 'Sukhumvit Set',
+            fontDisplay: 'swap',
+            src: "local('Sukhumvit Set')",
+            unicodeRange: 'U+0E01-0E5B, U+200C-200D, U+25CC',
+          },
+        },
       ])
     },
   ],

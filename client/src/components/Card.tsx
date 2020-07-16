b340b2cd8b90a7194a25b85a56412f7373e84a6c
iammarkps <markkung08@gmail.com>
2020-07-16 12:17:59 +0700

    :lipstick: Change card roundness

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,5 +1,5 @@
 export const Card = ({ children, ...restProps }) => (
-  <div className="shadow p-8 bg-white rounded" {...restProps}>
+  <div className="shadow-md p-8 bg-white rounded-md" {...restProps}>
     {children}
   </div>
 )

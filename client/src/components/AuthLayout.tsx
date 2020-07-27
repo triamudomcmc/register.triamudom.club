00e2350b957c7107847824870840e1b8834eabd0
iammarkps <markkung08@gmail.com>
2020-07-28 00:10:38 +0700

    :lipstick: Improve auth ui

--- client/src/components/AuthLayout.tsx
+++ client/src/components/AuthLayout.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Card } from './Card'
 
 export const AuthLayout = ({ title, children }) => (
-  <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50">
-    <div className="max-w-lg font-display">
+  <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
+    <div className="sm:mx-auto sm:w-full sm:max-w-sm font-display">
       <div>
         <img src="/assets/logo/logo.png" className="w-64 mx-auto"></img>
       </div>
